59d776b4f565bdf6aa7d3d5a5af7bc7a6b2a7e9c
Léo Kyrillos <leokyrillos@gmail.com>
2022-05-04 12:10:50 -0400

    Alteracoes realizadas

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -17,12 +17,12 @@ describe('Funcionalidades página de produtos', () => {
 
     it.only('Deve adicionar um item ao carrinho', () => {
 
-        var quantidade = 11
+        var quantidade = 1
 
         cy.get('[class="product-block grid"]')
             .contains('Ariel Roll Sleeve Sweatshirt')
             .click()
-        cy.get('.button-variable-item-XL')
+        cy.get('.button-variable-item-L')
             .click()
         cy.get('.button-variable-item-Green')
             .click()
@@ -31,8 +31,11 @@ describe('Funcionalidades página de produtos', () => {
             .type(quantidade)
         cy.get('.single_add_to_cart_button')
             .click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        //cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+
+        //Por vezes o site muda e o assert apresenta erro. Há vezes em que o site não apresenta a quantidade de itens na resposta de adicionado ao carrinho.
+        cy.get('.woocommerce-message').should('contain', 'Ariel Roll Sleeve Sweatshirt” foi adicionado no seu carrinho.')
+        //cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
 
     });
 
