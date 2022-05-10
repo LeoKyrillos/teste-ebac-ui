2a267534d8a12cf4527a9ac0e159b3ddd8e8eeac
Léo Kyrillos <leokyrillos@gmail.com>
2022-05-10 16:27:53 -0400

    Alteração do cadastro com massa de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types = "cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidede Endereços - Faturamento e Entrega', () => {
 
@@ -13,7 +15,28 @@ describe('Funcionalidede Endereços - Faturamento e Entrega', () => {
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
 
-        
+        EnderecoPage.editarEnderecoFaturamento('Manoel', 'Oliveira', 'BBV', 'Brasil', 'Rua 2', 23, 'Olinda', 'Pernambuco', '12350484', '999854562', 'manu@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
         
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
+
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidede,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefone,
+            dadosEndereco[2].email            
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+        
+    });
+
 });
